959f93034d483e2d066b389067c8f07c5fcb3307
JusterGonzalez4567 <134667199+JusterGonzalez4567@users.noreply.github.com>
2024-10-29 17:44:48 -0600

    cambios

--- appweb.py
+++ appweb.py
@@ -8,8 +8,15 @@ page = st.sidebar.selectbox("Navegación", ["Inicio", "Acerca de"])
 
 # Lógica de navegación entre páginas
 if page == "Inicio":
-    st.title("Página de Inicio")
+    st.title("Página de Inicio")    
     st.write("Bienvenido a la página principal.")
 elif page == "Acerca de":
     st.title("Acerca de")
     st.write("Esta es una aplicación con múltiples páginas.")
+
+if page == "ventas":
+    st.title("Ventas")    
+    st.write("Ventas")
+elif page == "ChatBot":
+    st.title("ChatBot")
+    st.write("Bienvenidos")
